245bd5e747098fcbbb21c6ad5ad9331dad939b9f
zy <181332929@qq.com>
2020-12-21 23:17:52 +0800

    配合vue的父子传参

--- eureka/src/main/java/com/zy/EurekaApp.java
+++ eureka/src/main/java/com/zy/EurekaApp.java
@@ -1,13 +1,15 @@
 package com.zy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApp {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApp.class,args);
+        SpringApplication.run(EurekaApp.class, args);
     }
 }
